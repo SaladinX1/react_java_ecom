33a8b1bcb04dccb51776a8b2d94172ecd71f6b08
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-06-22 14:47:58 +0200

    Commit app.js (application central)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Header_nav from './Components/Header_nav';
+import Log from './Views/Log'
+import Articles from './Views/Articles'
+import Commande_Ok from './Views/Commande_Ok'
+import Commandes from './Views/Commandes'
+import GestionAchats from './Views/GestionAchats'
+import GestionAdmin from './Views/GestionAdmin'
+import GestionArticles from './Views/GestionArticles'
+import HistoriqueCmd from './Views/HistoriqueCmd'
+import Paiement from './Views/Paiement'
 
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header_nav  /> 
+
+      <Routes>
+       
+        <Route path='/log' element={<Log />}></Route>
+        <Route path='/commande-ok' element={<Commande_Ok />} ></Route> 
+         <Route path='/paiement' element={<Paiement />} ></Route>
+         <Route path='/historique-commande' element={<HistoriqueCmd />} ></Route> 
+          <Route path='/articles' element={<Articles />} ></Route> 
+         <Route path='/commandes' element={<Commandes />} ></Route> 
+         <Route path='/gestion-achats' element={<GestionAchats />} ></Route> 
+         <Route path='/gestion-articles' element={<GestionArticles />} ></Route> 
+         <Route path='/gestion-admin' element={<GestionAdmin />} ></Route> 
+      </Routes>
     </div>
   );
 }
